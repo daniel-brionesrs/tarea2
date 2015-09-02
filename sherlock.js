082f2da348f095079d48572123221c316180f558
daniel.brionesrs <daniel.brionesrs@udlap.mx>
2015-09-02 12:22:06 -0500

    Tarea 2
    Tarea 2

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,22 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+cadena = _INPUT.split("\n");
+
+for(i = 0; i < cadena[0]; i++)
+{
+suma = 0; 
+	temp = cadena[i +1].split(" ");
+
+	for(k = temp[0]; k <= temp[1]; k++){
+		num = Math.sqrt(k);
+		comparando = Math.ceil(num);
+
+			if (num == comparando) {
+			suma = suma +1;
+			//console.log("suma = " + suma + "\n");
+			}
+	}
+	console.log("Intervalo " + (i+1) + " = " + suma);
+}
+
